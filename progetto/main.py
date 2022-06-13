4f9c70469a894e1c53e3a1b77a9b57af895d3d69
Matteo Cavada <cvd00@insicuri.net>
2022-06-14 01:11:52 +0200

    Correzioni

--- progetto/main.py
+++ progetto/main.py
@@ -6,6 +6,9 @@ import numpy as np
 from numpy.linalg import norm
 import random
 
+import progetto.kfold as kfold
+
+
 with h5py.File("data/usps.h5", 'r') as hf:
     train = hf.get('train')
     X_tr = train.get('data')[:]
@@ -18,7 +21,7 @@ with h5py.File("data/usps.h5", 'r') as hf:
 
     
 def kernel_poly(x1, x2, exp=2):
-    return (1 + x1.T.dot(x2)) ** exp
+    return (1 + x1.dot(x2)) ** exp
 
 def kernel_gauss(x1, x2, gamma=0.25):
     return math.exp(-(norm(x1-x2)**2)/(2*gamma))
@@ -56,18 +59,29 @@ def pegasos(X, Y, for_digit, kernel, lambd, T) -> Callable:
     while (t < T):
         idx = random.randint(0, len(X)-1)
         x, y = X[idx], Y[idx]
-        
+
         if t%1000 == 0:
             print(t, len(alpha))
             
         y = 1 if y == for_digit else -1
+
+        prediction  = (1/(lambd*t))
+        prediction *= sum(ys * kernel(xs, x) for xs,ys in alpha)
+
+        # print(for_digit, Y[idx], prediction)
         
-        prediction  = y / (lambd*t)
-        prediction *= sum(ys + kernel(x, xs) for xs,ys in alpha)
+        # if sign(prediction) != y:
+        #     alpha.append((x,y))
+        # prediction  = y / (lambd*t)
+        # prediction *= sum(ys * kernel(x, xs) for xs,ys in alpha)
 
-        if prediction <= 1:
+        if y * prediction < 1:
             alpha.append((x,y))
-
+            
+            print(f"Wrong: {for_digit} {Y[idx]} {prediction}")
+        else:
+            pass
+        
         t += 1
 
     print(t, len(alpha))
@@ -77,7 +91,7 @@ def pegasos(X, Y, for_digit, kernel, lambd, T) -> Callable:
     )
 
 
-def pegasos_predictors(X, Y, lambd, T) -> List[Callable]:
+def pegasos_predictors(X, Y, lambd, T, kernel) -> List[Callable]:
     """
     Train predictors for all 10 digits.
     """
@@ -86,7 +100,7 @@ def pegasos_predictors(X, Y, lambd, T) -> List[Callable]:
     for digit in range(0, 10):
         print(f"Building predictor for digit {digit}")
         
-        predictor = pegasos(X_tr,y_tr,digit,kernel_gauss,lambd,T)
+        predictor = pegasos(X,Y,digit,kernel,lambd,T)
         predictors.append(predictor)
 
     return predictors
@@ -119,67 +133,69 @@ def test_error(X, Y, predictors: List[Callable]) -> float:
 # =========================================================== #
 # =========================================================== #
 
-import progetto.kfold as kfold
 
-
-def single_train_and_test(X, Y, lambd, T) -> Tuple[float, int, float]:
+def single_train_and_test(X, Y, lambd, T, kernel) -> Tuple[float, int, float]:
     """
     Performs an estimation of test error using 5-fold cross-validation.
     """
     print(f"Running on: lambda={lambd} and T={T}")
     
-    test_errors   = list()
-    running_times = list()
+    test_errors = list()
+    times       = list()
     
-    for (xtr, ytr), (xte, yte) in kfold.generate_folds(all_X, all_Y, 5):
+    for (xtr, ytr), (xte, yte) in kfold.generate_folds(X, Y, 5):
         start = time.time()
 
         # Build predictors for all digits
-        predictors = pegasos_predictors(xtr, ytr, lambd, T)
+        predictors = pegasos_predictors(xtr, ytr, lambd, T, kernel)
         print(f"Predictors generation took: {int(time.time() - start)} seconds")
         
         test_errors.append(
             test_error(xte, yte, predictors)
         )
         
-        running_times.append(int(time.time() - start))
+        times.append(int(time.time() - start))
         
         print(f"Single round test-error for lambda={lambd}, T={T}: " +
-              f"{test_errors[-1]} in {running_times[-1]} seconds")
+              f"{test_errors[-1]} in {times[-1]} seconds")
 
         
     print("Completed 5-fold train and test")
-    print(f"Average duration of single round: {int(sum(times)/len(times))}")
-    print(f"Total time: {sum(times)}")
+    print(f"Average test error: {sum(test_errors) / len(test_errors)}")
+    print(f"Average duration of single round: {int(sum(times)/len(times))} seconds")
+    print(f"Total time: {sum(times)} seconds")
     
     return (lambd, T, sum(test_errors) / len(test_errors))
 
-# =========================================================== #
-
-# single_train_and_test(all_X, all_Y, 0.01, 2500)
 
 # =========================================================== #
 import time
 
 __trainset_size = 4 * (len(all_X) // 5)
-Ts = [int(x * __trainset_size) for x in [0.1, 0.5, 1, 2]]
+# Ts = [int(x * __trainset_size) for x in [0.1, 0.5, 1]]
+Ts = [int(x * __trainset_size) for x in [0.5, 0.1, 0.5, 1]]
 
 results = list()
 last_time = time.time()
 
+# for kernel in [kernel_gauss, kernel_poly, lambda x,y:kernel_poly(x,y,7)]:
+    # kernel_time = time.time()
+    
 for T in Ts:
-    for lambd in [10**-10, 0.1, 1, 10]:
+    for lambd in [10**-6, 10**-5, 10**-4, 10**-3]:
         lambd_time = time.time()
-        
+
         results.append(
-            single_train_and_test(all_X, all_Y, lambd, T)
+            single_train_and_test(all_X, all_Y, lambd, T,
+                                  # lambda x,y:kernel_poly(x,y,7))
+                                  kernel_gauss)
         )
 
-        with open("results.csv", "a") as f:
+        with open("results-gausskernel.csv", "a") as f:
             f.write(f"{lambd}, {T}, {results[-1][-1]}\n")
 
-        print(f"Total lambda time: {time.time() - lambd_time}\n\n")
+        print(f"Total time: {time.time() - lambd_time} seconds\n\n")
 
-    print(f"Total time: {time.time() - last_time}")
+print(f"Total time: {time.time() - last_time}")
 # =========================================================== #
 
